07730ad4df9c6f2449714688c5cb5118afa9a170
misha 🐦‍🔥 <146671001+ovnanova@users.noreply.github.com>
2024-10-06 00:04:25 -0700

    simplified event handling

--- oneko_touch.js
+++ oneko_touch.js
@@ -1,1 +1,1 @@
-!function(){function e(e,t){e.target.dispatchEvent(new MouseEvent("mousemove",{view:window,clientX:t.clientX,clientY:t.clientY,bubbles:!0}))}["touchstart","touchmove"].forEach(t=>addEventListener(t,n=>{t=="touchmove"||n.target.style.backgroundImage?.includes("oneko.gif")?n.preventDefault():0,e(n,n.touches[0])},{passive:!1}))}();
+!function(){function e(e,t){e.target.dispatchEvent(new MouseEvent("mousemove",{view:window,clientX:t.clientX,clientY:t.clientY,bubbles:!0}))}addEventListener("touchmove",t=>{t.preventDefault(),e(t,t.touches[0])},{passive:!1})}();
